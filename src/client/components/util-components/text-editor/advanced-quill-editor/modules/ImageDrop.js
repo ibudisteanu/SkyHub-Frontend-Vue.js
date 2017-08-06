2b762404cf65055d687f5da0549d7b7da273008c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-08-07 01:09:20 +0300

    ImageDrop & Image Upload functional

--- src/client/components/util-components/text-editor/advanced-quill-editor/modules/ImageDrop.js
+++ src/client/components/util-components/text-editor/advanced-quill-editor/modules/ImageDrop.js
@@ -3,8 +3,13 @@
  * and paste images from clipboard (Works on Chrome, Firefox, Edge, not on Safari)
  * @see https://quilljs.com/blog/building-a-custom-module/
  */
-export class ImageImport {
+export class ImageDrop {
 
+    /**
+     * Instantiate the module given a quill instance and any options
+     * @param {Quill} quill
+     * @param {Object} options
+     */
     constructor(quill, options = {}) {
         // save the quill reference
         this.quill = quill;
@@ -16,13 +21,28 @@ export class ImageImport {
         this.quill.root.addEventListener('paste', this.handlePaste, false);
     }
 
+    /**
+     * Handler for drop event to read dropped files from evt.dataTransfer
+     * @param {Event} evt
+     */
     handleDrop(evt) {
         evt.preventDefault();
         if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length) {
+            if (document.caretRangeFromPoint) {
+                const selection = document.getSelection();
+                const range = document.caretRangeFromPoint(evt.clientX, evt.clientY);
+                if (selection && range) {
+                    selection.setBaseAndExtent(range.startContainer, range.startOffset, range.startContainer, range.startOffset);
+                }
+            }
             this.readFiles(evt.dataTransfer.files, this.insert.bind(this));
         }
     }
 
+    /**
+     * Handler for paste event to read pasted files from evt.clipboardData
+     * @param {Event} evt
+     */
     handlePaste(evt) {
         if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
             this.readFiles(evt.clipboardData.items, dataUrl => {
@@ -40,11 +60,20 @@ export class ImageImport {
         }
     }
 
+    /**
+     * Insert the image into the document at the current cursor position
+     * @param {String} dataUrl  The base64-encoded image URI
+     */
     insert(dataUrl) {
         const index = (this.quill.getSelection() || {}).index || this.quill.getLength();
         this.quill.insertEmbed(index, 'image', dataUrl, 'user');
     }
 
+    /**
+     * Extract image URIs a list of files from evt.dataTransfer or evt.clipboardData
+     * @param {File[]} files  One or more File objects
+     * @param {Function} callback  A function to send each data URI to
+     */
     readFiles(files, callback) {
         // check each file for an image
         [].forEach.call(files, file => {
