4a373596f677468f189b9477dcfe5198f5cba1fe
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-08-24 19:11:55 +0300

    Display Breadcrumbs

--- src/client/components/util-components/UI/breadcrumbs/DisplayBreadcrumbs.component.vue
+++ src/client/components/util-components/UI/breadcrumbs/DisplayBreadcrumbs.component.vue
@@ -9,40 +9,35 @@
 
 <template>
 
-  <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+  <ol class="breadcrumb">
 
-    <li v-if="this.homeAvailable" key="breadcrumb_home" itemprop="itemListElement"   itemscope itemtype="http://schema.org/ListItem">
-      <router-link to="/#top" itemscope itemtype="http://schema.org/Thing" itemprop="item" >
+    <li v-if="this.homeAvailable" key="breadcrumb_home" >
+      <router-link to="/#top">
           <i class="fa fa-home"></i>
-          <span itemprop="name">Home</span>
+          <span>Home</span>
       </router-link>
-      <meta itemprop="position" content="1" />
     </li>
 
     <li   v-for="(breadcrumb, index) in breadcrumbs"
           :key="'breadcrumb_'+breadcrumb.name+'_'+index"
-
-          itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"
     >
-      <router-link :key="'breadcrumb_'+breadcrumb.name+'_'+index" :to="'/'+breadcrumb.url+'#top'"   itemscope itemtype="http://schema.org/Thing" itemprop="item">
-          <span itemprop="name">
+      <router-link :key="'breadcrumb_'+breadcrumb.name+'_'+index" :to="'/'+breadcrumb.url+'#top'"   >
+          <span >
               {{typeof breadcrumb.name === 'string' && breadcrumb.name.length > 30 ? breadcrumb.name.substr(0,30)+'...' :  breadcrumb.name}}
           </span>
       </router-link>
-      <meta itemprop="position" :content="index+1 + (this.homeAvailable ? 1 : 0)" />
     </li>
 
-    <li  class="active" key="breadcrumb_current_Page"         itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-      <router-link key="breadcrumb_current_Page" :to="'/'+currentPageUrl+'#top'"    itemscope itemtype="http://schema.org/Thing" itemprop="item">
+    <li  class="active" key="breadcrumb_current_Page">
+      <router-link key="breadcrumb_current_Page" :to="'/'+currentPageUrl+'#top'">
 
-        <i v-if="this.getPageTitle === 'Home'" class="fa fa-home"> </i>
+            <i v-if="this.getPageTitle === 'Home'" class="fa fa-home"> </i>
 
-        <span itemprop="name">
-            {{typeof this.getPageTitle === 'string' && this.getPageTitle.length > 30 ? getPageTitle.substr(0,30)+'...' :  this.getPageTitle}}
-        </span>
+            <span>
+                {{typeof this.getPageTitle === 'string' && this.getPageTitle.length > 30 ? getPageTitle.substr(0,30)+'...' :  this.getPageTitle}}
+            </span>
 
       </router-link>
-      <meta itemprop="position" :content="breadcrumbs.length+(this.homeAvailable ? +1 : 0)">
     </li>
 
 
