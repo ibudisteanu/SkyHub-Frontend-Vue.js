8a91645fe9cf66dbf0fb2af52393db54c8d6edef
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-11-20 12:53:00 +0200

    AddForum parent bug fix

--- src/modules/forums/forums/components/AddForum.form.vue
+++ src/modules/forums/forums/components/AddForum.form.vue
@@ -215,7 +215,7 @@
 
                 if (!bValidationError)
                     try{
-                        let answer = await this.$store.dispatch('CONTENT_FORUMS_SUBMIT_ADD',{ parentId:this.parentId || this.parentId, name: this.name, title: this.title||this.name||'', description:this.description, keywords:this.keywords,
+                        let answer = await this.$store.dispatch('CONTENT_FORUMS_SUBMIT_ADD',{ parentId:this.parentId || this.parentIdProp, name: this.name, title: this.title||this.name||'', description:this.description, keywords:this.keywords,
                                                                 country: this.countryCode || this.localization.countryCode, language:'',  city: this.city || this.localization.city, latitude: this.latitude || this.localization.latitude, longitude: this.longitude || this.localization.longitude,
                                                                 timezone: this.localization.timeZone})
 
