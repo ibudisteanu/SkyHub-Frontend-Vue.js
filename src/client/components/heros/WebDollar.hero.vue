a041bccba5cabde681ec468ec8f1f4792ff36251
alexandru.panait <alexandru.panait@vivacredit.ro>
2017-12-29 15:07:15 +0200

    fix style

--- src/client/components/heros/WebDollar.hero.vue
+++ src/client/components/heros/WebDollar.hero.vue
@@ -7,13 +7,13 @@
 
             <h1 class="fadeIn fadeIn2">Web Dollar</h1>
             <h3 class="fadeIn fadeIn3">Currency of the Internet</h3>
-            <div class="btn-cont">
-                <a onclick="maximizeChat()" class="btn">
+            <div class='btn-cont btnPosition fadeIn fadeIn4'>
+                <a class='btn'>
                     Start Mining
-                    <span class="line-1"></span>
-                    <span class="line-2"></span>
-                    <span class="line-3"></span>
-                    <span class="line-4"></span>
+                    <span class='line-1'></span>
+                    <span class='line-2'></span>
+                    <span class='line-3'></span>
+                    <span class='line-4'></span>
                 </a>
             </div>
 
