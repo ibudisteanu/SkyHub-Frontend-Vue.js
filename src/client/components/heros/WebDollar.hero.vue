32811032cb442936f3bad9477df228dc37c6519f
alexandru.panait <alexandru.panait@vivacredit.ro>
2018-01-02 15:41:27 +0200

    logo style

--- src/client/components/heros/WebDollar.hero.vue
+++ src/client/components/heros/WebDollar.hero.vue
@@ -3,7 +3,7 @@
     <div id="mainSection" class="fullSection">
         <div class="verticalAlignMiddle alignCenter modifyTop verticalAlignMiddleMobileFix">
 
-            <img src="/public/WebDollar-logo-black.png" class="mainLogo fadeIn">
+            <img src="/public/WebDollar-logo-white.png" class="mainLogo fadeIn">
 
             <h1 class="fadeIn fadeIn2">Web Dollar</h1>
             <h3 class="fadeIn fadeIn3">Currency of the Internet</h3>
