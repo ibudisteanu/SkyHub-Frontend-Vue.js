9b0e9c0344c5383631552c6205df541de2d48c91
alexandru.panait <alexandru.panait@vivacredit.ro>
2018-01-03 23:16:04 +0200

    fix bugs

--- src/client/components/heros/PeerToPeer.hero.vue
+++ src/client/components/heros/PeerToPeer.hero.vue
@@ -7,7 +7,7 @@
                 <h1 class="alignCenter">Peer-to-Peer Network</h1>
                 <h3 >On the following map you can see all your mining group connections in real time</h3>
 
-                <div id="webDollarMap"></div>
+                <div id="WebDollarMap"></div>
 
             </div>
         </div>
