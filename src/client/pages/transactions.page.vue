da45a90e0f7d6077cc155034efb1e418d1d9b6ef
alexandru.panait <alexandru.panait@vivacredit.ro>
2018-01-04 23:40:25 +0200

    transactions

---
+++ src/client/pages/transactions.page.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div id="transactions">
+        <div id="transactionContainer">
+            <div class="transactionInfo">
+                <span class="id">53143</span>
+                <span class="time">1 min ago</span>
+                <span class="transactions">3 transactipons</span>
+                <span class="minedBy">mined by fas as gsag sagsg as gasd gsa gsa</span>
+                <span class="size">400bytes</span>
+            </div>
+            <div class="transactionInfo">
+                <span class="id">53143</span>
+                <span class="time">1 min ago</span>
+                <span class="transactions">3 transactipons</span>
+                <span class="minedBy">mined by fas as gsag sagsg as gasd gsa gsa</span>
+                <span class="size">400bytes</span>
+            </div>
+            <div class="transactionInfo">
+                <span class="id">53143</span>
+                <span class="time">1 min ago</span>
+                <span class="transactions">3 transactipons</span>
+                <span class="minedBy">mined by fas as gsag sagsg as gasd gsa gsa</span>
+                <span class="size">400bytes</span>
+            </div> <div class="transactionInfo">
+            <span class="id">53143</span>
+            <span class="time">1 min ago</span>
+            <span class="transactions">3 transactipons</span>
+            <span class="minedBy">mined by fas as gsag sagsg as gasd gsa gsa</span>
+            <span class="size">400bytes</span>
+        </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default{
+
+
+    }
+
+</script>
