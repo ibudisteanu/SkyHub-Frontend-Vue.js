ee21b9de04a6c498f71e706284cb46f7da07bd9d
alexandru.panait <alexandru.panait@vivacredit.ro>
2018-01-08 01:33:05 +0200

    faq section

--- src/client/components/heros/faq.hero.vue
+++ src/client/components/heros/faq.hero.vue
@@ -6,7 +6,7 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                1. Question
+                1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mollis nisi quis quam luctus, ut.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -15,7 +15,7 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                2. Question
+                2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu felis mauris. Duis est turpis.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -24,7 +24,7 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                1. Question
+                3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce diam orci, viverra eget velit in.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -33,7 +33,7 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                1. Question
+                4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt vel dui in consequat. Etiam.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -42,7 +42,7 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                1. Question
+                5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ante augue, tincidunt et nunc vitae.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -51,39 +51,13 @@
 
         <collapse :selected="false">
             <div slot="collapse-header">
-                1. Question
+                6. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat consequat tincidunt. Integer vulputate erat.
             </div>
             <div slot="collapse-body">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </div>
         </collapse>
 
-        <collapse :selected="false">
-            <div slot="collapse-header">
-                1. Question
-            </div>
-            <div slot="collapse-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-        </collapse>
-
-        <collapse :selected="false">
-            <div slot="collapse-header">
-                1. Question
-            </div>
-            <div slot="collapse-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-        </collapse>
-
-        <collapse :selected="false">
-            <div slot="collapse-header">
-                1. Question
-            </div>
-            <div slot="collapse-body">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </div>
-        </collapse>
     </div>
 </template>
 
