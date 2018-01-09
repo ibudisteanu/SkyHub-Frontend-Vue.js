d0e4e86f4c7db0467a074df97c0c77308e21f5b7
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-09 06:10:38 +0200

    minor bug fix

--- public/WebDollar-dist/WebDollar-User-Interface-bundle.js
+++ public/WebDollar-dist/WebDollar-User-Interface-bundle.js
@@ -17400,7 +17400,7 @@ exports.default = {
 if (module.exports.__esModule) module.exports = module.exports.default
 var __vue__options__ = (typeof module.exports === "function"? module.exports.options: module.exports)
 if (__vue__options__.functional) {console.error("[vueify] functional components are not supported and should be defined in plain js files using render functions.")}
-__vue__options__.render = function render () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"show-balance-span"},[_vm._v("\n    "+_vm._s((this.balances !== null && this.balances.hasOwnProperty(this.currency)) ? Math.round(this.balances[this.currency] * 100000)/100000 : 0)+"\n")])}
+__vue__options__.render = function render () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"show-balance-span"},[_vm._v("\n    "+_vm._s((this.balances !== null && this.balances !== undefined && this.balances.hasOwnProperty(this.currency)) ? Math.round(this.balances[this.currency] * 100000)/100000 : 0)+"\n")])}
 __vue__options__.staticRenderFns = []
 if (module.hot) {(function () {  var hotAPI = require("vue-hot-reload-api")
   hotAPI.install(require("vue"), true)
