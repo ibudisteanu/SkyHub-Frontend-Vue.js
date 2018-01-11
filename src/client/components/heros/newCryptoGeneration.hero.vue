c8c94152de17799f7f356cd8fa2208072194f11a
alexandru.panait <ionutalexpanait@gmail.com>
2018-01-11 13:18:14 +0200

    currency

--- src/client/components/heros/newCryptoGeneration.hero.vue
+++ src/client/components/heros/newCryptoGeneration.hero.vue
@@ -34,7 +34,7 @@
                                 <path style=" fill:#fec02c;" d="M 34.585938 14.585938 L 21.015625 28.171875 L 15.414063 22.585938 L 12.585938 25.414063 L 21.019531 33.828125 L 37.414063 17.414063 Z "/>
                             </g>
                         </svg>
-                        <span class="newCryptoText">Light Blockchain (using PPoW)</span>
+                        <span class="newCryptoText">The First pool which can be managed</span>
                     </div>
                 </div>
                 <div>
@@ -55,6 +55,15 @@
                             </g>
                         </svg>
                         <span class="newCryptoText">Simple and Secure Transactions</span>
+                    </div>
+                    <div class="checkItem">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" version="1.1" width="30px" height="30px">
+                            <g id="surface1">
+                                <path style=" fill:rgb(41, 41, 41);" d="M 36 42 L 12 42 C 8.6875 42 6 39.3125 6 36 L 6 12 C 6 8.6875 8.6875 6 12 6 L 36 6 C 39.3125 6 42 8.6875 42 12 L 42 36 C 42 39.3125 39.3125 42 36 42 Z "/>
+                                <path style=" fill:#fec02c;" d="M 34.585938 14.585938 L 21.015625 28.171875 L 15.414063 22.585938 L 12.585938 25.414063 L 21.019531 33.828125 L 37.414063 17.414063 Z "/>
+                            </g>
+                        </svg>
+                        <span class="newCryptoText">Light Blockchain (using PPoW)</span>
                     </div>
                 </div>
             </div>
