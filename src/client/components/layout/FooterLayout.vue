b3d647df8135c9361f5f16688cab18090af9202b
alexandru.panait <ionutalexpanait@gmail.com>
2018-02-17 22:32:27 +0200

    adding redit

--- src/client/components/layout/FooterLayout.vue
+++ src/client/components/layout/FooterLayout.vue
@@ -3,14 +3,17 @@
 
         <span id="copyright">WebDollar is an <span class="secondCopyRight"> <a href="http://github.com/WebDollar/">open-source</a> decentralized p2p protocol.</span></span>
         <div id="socialLinks">
-            <a href="https://facebook.com/webdollar.io"  target="_blank">
+            <a href="https://facebook.com/webdollar.io" target="_blank">
                 <img class="socialImage" src="public/assets/images/facebook-logo.svg">
             </a>
-            <a href="https://t.me/WebDollar"  target="_blank">
+            <a href="https://t.me/WebDollar" target="_blank">
                 <img class="socialImage" src="public/assets/images/telegram.svg">
             </a>
-            <a href="https://github.com/WebDollar"  target="_blank">
+            <a href="https://github.com/WebDollar" target="_blank">
                 <img class="socialImage" src="public/assets/images/github-logo.svg">
+            </a>
+            <a href="https://www.reddit.com/r/webdollar/" target="_blank">
+                <img class="socialImage" src="public/assets/images/reddit.svg">
             </a>
         </div> 
 
