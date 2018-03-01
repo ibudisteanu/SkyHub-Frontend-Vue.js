f05bbc2256aabb52e55b9c7c2c364f5a4bee4ca0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-03-01 04:14:52 +0200

    Network Timestamp fixed

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -1935,8 +1935,8 @@ consts.BLOCKCHAIN = {
 
     TIMESTAMP:{
         VALIDATION_NO_BLOCKS: 10,
-        NETWORK_ADJUSTED_TIME_MAXIMUM_BLOCK_OFFSET: 10*60*1000,
-        NETWORK_ADJUSTED_TIME_NODE_MAX_UTC_DIFFERENCE: 10*60*1000,
+        NETWORK_ADJUSTED_TIME_MAXIMUM_BLOCK_OFFSET: 10*60,
+        NETWORK_ADJUSTED_TIME_NODE_MAX_UTC_DIFFERENCE: 10*60,
     },
 
     DIFFICULTY_TIME : 200, //in s, timestamp in UNIX format
@@ -24747,7 +24747,7 @@ class InterfaceBlockchainBlock {
         if (this.height >= __WEBPACK_IMPORTED_MODULE_4_consts_const_global__["a" /* default */].BLOCKCHAIN.HARD_FORKS.TEST_NET_3.DIFFICULTY_HARD_FORK)
             if ( this.blockValidation.blockValidationType['validation-timestamp-adjusted-time'] === true ) {
 
-                if (this.timeStamp < this.blockchain.timestamp.networkAdjustedTime - __WEBPACK_IMPORTED_MODULE_2_common_blockchain_global_Blockchain_Genesis__["a" /* default */].timeStampOffset + __WEBPACK_IMPORTED_MODULE_4_consts_const_global__["a" /* default */].BLOCKCHAIN.TIMESTAMP.NETWORK_ADJUSTED_TIME_MAXIMUM_BLOCK_OFFSET)
+                if (this.timeStamp > this.blockchain.timestamp.networkAdjustedTime - __WEBPACK_IMPORTED_MODULE_2_common_blockchain_global_Blockchain_Genesis__["a" /* default */].timeStampOffset + __WEBPACK_IMPORTED_MODULE_4_consts_const_global__["a" /* default */].BLOCKCHAIN.TIMESTAMP.NETWORK_ADJUSTED_TIME_MAXIMUM_BLOCK_OFFSET)
                     throw "Timestamp of block is less than the network-adjusted time "+this.timeStamp+"  "+" "+this.blockchain.timestamp.networkAdjustedTime+" "+__WEBPACK_IMPORTED_MODULE_4_consts_const_global__["a" /* default */].BLOCKCHAIN.TIMESTAMP.NETWORK_ADJUSTED_TIME_MAXIMUM_BLOCK_OFFSET
             }
 
