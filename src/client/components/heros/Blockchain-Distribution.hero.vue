43cca2a8e02f8dcf1abf580c9aa61eff4ac602fc
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-03-15 04:22:17 +0200

    Minor Update & bundle changes

--- src/client/components/heros/Blockchain-Distribution.hero.vue
+++ src/client/components/heros/Blockchain-Distribution.hero.vue
@@ -18,40 +18,14 @@
                     <div class="stats">
                         <div>
                             <span v-show="!this.loaded" class="value">
-                                 <div class="minningSpinner">
-                                    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                     width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-                                      <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-                                        <animateTransform attributeType="xml"
-                                          attributeName="transform"
-                                          type="rotate"
-                                          from="0 25 25"
-                                          to="360 25 25"
-                                          dur="0.6s"
-                                          repeatCount="indefinite"/>
-                                        </path>
-                                    </svg>
-                                </div>
+                                <loading-spinner />
                             </span>
                             <span v-show="this.loaded" class="value">{{this.distributionAmount}}</span>
                             <span class="description">Current Distribution</span>
                         </div>
                         <div>
                             <span v-show="!this.loaded" class="value">
-                                 <div class="minningSpinner">
-                                    <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                     width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-                                      <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-                                        <animateTransform attributeType="xml"
-                                          attributeName="transform"
-                                          type="rotate"
-                                          from="0 25 25"
-                                          to="360 25 25"
-                                          dur="0.6s"
-                                          repeatCount="indefinite"/>
-                                        </path>
-                                  </svg>
-                                </div>
+                                 <loading-spinner />
                             </span>
                             <span v-show="this.loaded" class="value">{{this.distributionBlocks}}</span>
                             <span class="description">Mined blocks</span>
@@ -75,9 +49,15 @@
 
 <script>
 
+    import LoadingSpinner from "client/components/UI/elements/Loading-Spinner.vue"
+
     export default{
 
         name: "BlockchainDistribution",
+
+        components:{
+            LoadingSpinner,
+        },
 
         data() {
             return {
--- src/client/components/heros/WebDollar.hero.vue
+++ src/client/components/heros/WebDollar.hero.vue
@@ -5,7 +5,7 @@
 
             <img src="/public/WebDollar-logo-white.png" class="mainLogo fadeIn">
 
-            <h1 class="fadeIn fadeIn2">WebDollar <b class="testnet">TEST NET# 3.1</b></h1>
+            <h1 class="fadeIn fadeIn2">WebDollar <b class="testnet">TEST NET# 3.2</b></h1>
             <h2 v-show="this.maintenance" class="fadeIn fadeIn2"><b class="testnet">UNDER MAINTENANCE</b></h2>
             <h3 class="fadeIn fadeIn3" :style="{marginTop: !this.mainNet ? '0': '30px'}">Currency of the Internet</h3>
 
@@ -15,20 +15,7 @@
 
                 <h5 class="fadeIn fadeIn3" v-if="this.loaded === false">
                     <span class="alreadyMining">You are already mining...</span>
-                    <div class="minningSpinner">
-                        <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                     width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-                      <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-                        <animateTransform attributeType="xml"
-                          attributeName="transform"
-                          type="rotate"
-                          from="0 25 25"
-                          to="360 25 25"
-                          dur="0.6s"
-                          repeatCount="indefinite"/>
-                        </path>
-                  </svg>
-                    </div>
+                    <loading-spinner />
                 </h5>
 
                 <div class='btn-cont btnPosition fadeIn fadeIn4'>
@@ -43,7 +30,7 @@
             </div>
 
             <div class="fadeIn fadeIn3" v-show="!this.mainNet">
-                <!--<Countdown end="April 1, 2018"></Countdown>-->
+                <!--<countdown end="April 1, 2018" /> -->
             </div>
 
         </div>
@@ -54,13 +41,15 @@
 <script>
 
     import Countdown from 'vuejs-countdown';
+    import LoadingSpinner from "client/components/UI/elements/Loading-Spinner.vue"
 
     export default{
 
         name: "WebDollarHero",
 
         components: {
-            "Countdown":Countdown
+            Countdown,
+            LoadingSpinner,
         },
 
         data: ()=>{
