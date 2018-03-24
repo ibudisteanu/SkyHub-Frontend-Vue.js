bc97f37578f7dfe71fd0479081063043e54c3dad
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-03-25 00:41:12 +0200

    Bounty Info refactoring

--- src/client/components/heros/bounty/info.form.vue
+++ src/client/components/heros/bounty/info.form.vue
@@ -11,182 +11,26 @@
 
         <div class="campaignInfo">
 
-            <div v-if="this.type === 'youtube'">
 
-                 <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing WebDollar/webd in description and content
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Views/10 + ThumbsUp - ThumbsDown*3 + Comments*2)/40
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
+            <span class="infoLine">
+                <b>Bounty registration:</b> {{ this.info.type }}
+            </span>
+            <span class="infoLine">
+                <b>Conditions of participation:</b> {{ this.info.requirement }}
+            </span>
+            <span class="infoLine">
+                <b>Score formula:</b> {{this.info.formula}}
+            </span>
+            <span class="infoLine">
+                <b>Ranking Update:</b> {{this.info.update}}
+            </span>
+            <span class="infoLine">
+                <b>Reward:</b> {{this.info.redeem}}
+            </span>
+            <span class="infoLine">
+                <b class="capitalize">{{this.type}} bounty Amount:</b> {{this.info.reward}}
+            </span>
 
-            </div>
-            <div v-if="this.type === 'facebook'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Register post link
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing WebDollar in description and content
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Likes + 3*shares + 1.5*comments)/30
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'instagram'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing #WebDollar in post
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Likes + 2*comments)/10
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'twitter'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing WebDollar in post
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Followers/100 + Friends/100 + Likes + Shares*5 + Comments*2)/10
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'telegram'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Be a member in the group
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Messages/100 + Invitations)
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every day
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'telegram RO'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Be a member in the group
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Messages/100 + Invitations)
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every day
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'reddit'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing WebDollar in description and content
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (ValueScore*2 + comments)/10
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
-            <div v-if="this.type === 'website'">
-
-                <span class="infoLine">
-                    <b>Bounty registration:</b> Automatically
-                </span>
-                <span class="infoLine">
-                    <b>Conditions of participation:</b> Containing WebDollar/webd in description and content
-                </span>
-                <span class="infoLine">
-                    <b>Score formula:</b> (Likes + 3*shares + 1.5*comments)/30
-                </span>
-                <span class="infoLine">
-                    <b>Classament Update:</b> Every 3 minutes
-                </span>
-                <span class="infoLine">
-                    <b>Reward:</b> You will be contacted by our team for providing your wallet Address
-                </span>
-                <span class="infoLine">
-                    <b class="capitalize">{{this.type}} bounty Ammount:</b> 10.000 WEBD
-                </span>
-
-            </div>
 
         </div>
 
@@ -205,9 +49,85 @@
         components: { Countdown },
 
         data: () => {
+
             return {
 
+                youtube:{
+                    type: "Automatically",
+                    requirement: 'Include "WebDollar" in description and title',
+                    formula: "( Views/10 + ThumbsUp - ThumbsDown*3 + Comments*2 )/40",
+                    update: "Every 3 minutes",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                facebook:{
+                    type: "Register post link",
+                    requirement: 'Include WebDollar in the content',
+                    formula: " (Likes + 3*shares + 1.5*comments)/30",
+                    update: "Every 3 minutes",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                instagram:{
+                    type: "Automatically",
+                    requirement: 'Include #WebDollar in post',
+                    formula: " (Likes + 3*shares + 1.5*comments)/30",
+                    update: "Every 3 minutes",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                twitter:{
+                    type: "Automatically",
+                    requirement: 'Include #WebDollar in post',
+                    formula: " (Followers/100 + Friends/100 + Likes + Shares*5 + Comments*2)/10",
+                    update: "Every 3 minutes",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                telegram:{
+                    type: "Automatically",
+                    requirement: 'Be a member in the group',
+                    formula: " (Messages/100 + Invitations) ",
+                    update: "Every day",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                telegramRO:{
+                    type: "Automatically",
+                    requirement: 'Be a member in the group',
+                    formula: " (Messages/100 + Invitations) ",
+                    update: "Every day",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                reddit:{
+                    type: "Automatically",
+                    requirement: 'Discus on the /r/WebDollar subreddit',
+                    formula: " (RedditScore*2 + comments)/10 ",
+                    update: "Every 3 minutes",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+                website:{
+                    type: "Automatically",
+                    requirement: 'Discus on the WebDollar in Title and Description',
+                    formula: " Google PageRank + Score*2 ",
+                    update: "Every day",
+                    redeem: "To redeem your bounty, you will need to contact the team for providing your wallet Address",
+                    reward: "10.000 WEBD",
+                },
+
             }
+        },
+
+        computed:{
+
+            info(){
+
+                if (this.type !== '')
+                    return this[this.type];
+            }
+
         },
 
         props:{
@@ -216,10 +136,6 @@
         },
 
         methods:{
-
-            showRules(){
-
-            }
 
         }
 
