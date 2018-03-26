21e88fd41921c09170cba20dc6c802e367a52bb1
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-03-26 20:01:16 +0300

    Countdown

--- src/client/components/UI/elements/Countdown.component.vue
+++ src/client/components/UI/elements/Countdown.component.vue
@@ -2,30 +2,7 @@
 
     <div>
 
-        COUNT DOWN {{this.deadline}}
-
-        <ul class="vuejs-countdown" v-if="this.message === ''">
-            <li>
-                <p class="digit">{{d}}</p>
-                <p class="text">days</p>
-            </li>
-            <li>
-                <p class="digit">{{h}}</p>
-                <p class="text">hours</p>
-            </li>
-            <li>
-                <p class="digit">{{m}}</p>
-                <p class="text">min</p>
-            </li>
-            <li>
-                <p class="digit">{{s}}</p>
-                <p class="text">Sec</p>
-            </li>
-        </ul>
-
-        <span class="finishedCount" v-if="this.message !== ''">
-            {{message}}
-        </span>
+        COUNT DOWN 
     </div>
 
 </template>
