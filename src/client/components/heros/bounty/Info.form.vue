1364392e23c16184d7d0c3c675bebd0acd97e701
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-03-26 20:52:15 +0300

    Countdown

--- src/client/components/heros/bounty/Info.form.vue
+++ src/client/components/heros/bounty/Info.form.vue
@@ -6,9 +6,33 @@
             <div class="verticalAlignMiddle">
                 <span class="countDownTitle">{{this.type}} bounty end:</span>
 
-                TEST
-                <test2 :deadline="this.info.deadline" ></test2>
                 <!--<countdown :deadline="this.info.deadline" ></countdown>-->
+
+
+                <ul class="vuejs-countdown" v-if="this.message === ''">
+                    <li>
+                        <p class="digit">{{d}}</p>
+                        <p class="text">days</p>
+                    </li>
+                    <li>
+                        <p class="digit">{{h}}</p>
+                        <p class="text">hours</p>
+                    </li>
+                    <li>
+                        <p class="digit">{{m}}</p>
+                        <p class="text">min</p>
+                    </li>
+                    <li>
+                        <p class="digit">{{s}}</p>
+                        <p class="text">Sec</p>
+                    </li>
+                </ul>
+
+                <span class="finishedCount" v-if="this.message !== ''">
+                    {{message}}
+                </span>
+
+
 
             </div>
         </div>
@@ -52,7 +76,6 @@
     import consts from "consts/constants"
     import SubmitLink from "./Submit-Link.form.vue";
     import countdown from "client/components/UI/elements/Countdown.component.vue"
-    import test2 from "client/components/UI/elements/Test2.component.vue"
 
     let axios = require('axios');
 
@@ -60,7 +83,6 @@
 
         components: {
             countdown,
-            test2,
             SubmitLink
         },
 
@@ -145,6 +167,15 @@
                     deadline: commonDeadline,
                 },
 
+
+                interval: undefined,
+
+                d: 0,
+                h: 0,
+                m: 0,
+                s: 0,
+                message: '',
+
             }
         },
 
@@ -176,15 +207,55 @@
         props:{
             type: {default: ''},
             onLinkSubmitted: {default: ()=>{}},
+            deadline : {default: "Sep 5, 2018 15:37:25" },
         },
 
         methods:{
 
+            countDown(){
+
+                let countDownDate = new Date(this.info.deadline).getTime();
+
+                // Get todays date and time
+                let now = new Date().getTime();
+
+                // Find the distance between now an the count down date
+                let distance = countDownDate - now;
+
+                // Time calculations for days, hours, minutes and seconds
+                this.d = Math.floor(distance / (1000 * 60 * 60 * 24));
+                this.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                this.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                this.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+                // Estetic redefine
+                if (this.h < 10)
+                    this.h = '0' + this.h;
+
+                if (this.d < 10)
+                    this.d = '0' + this.d;
+
+                if (this.m < 10)
+                    this.m = '0' + this.m;
+
+                if (this.s < 10)
+                    this.s = '0' + this.s;
+
+                // If the count down is finished, write some text
+                if (distance < 0) {
+                    clearInterval(this.interval);
+                    this.interval = undefined;
+                    this.message = 'FINISHED';
+                }
+
+            }
 
 
         },
 
         mounted(){
+
+            if (typeof window === "undefined") return false;
 
             setInterval(()=>{
 
@@ -196,6 +267,16 @@
                 this.refreshCountDownSeconds = Math.max(0, Math.floor((distance % (1000 * 60)) / 1000));
 
             }, 1000);
+
+
+            // Update the count down every 1 second
+            if (this.interval !== undefined)
+                clearInterval(this.interval);
+
+            this.interval = setInterval( this.countDown, 1000);
+
+            this.countDown();
+
 
         }
 
