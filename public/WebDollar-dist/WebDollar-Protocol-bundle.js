fc3e7594ee78288f65f33664401598a5a59923cb
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-01 05:08:29 +0300

    fallback nodes

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -27556,8 +27556,7 @@ class CLI{
             return;
         }
 
-        //let answer = await this.question('Command: ');
-        let answer = '9';
+        let answer = await this.question('Command: ');
 
         switch(answer.trim()) {
             case '1':
@@ -27595,7 +27594,7 @@ class CLI{
                 break;
         }
 
-        //await this._runMenu();
+        await this._runMenu();
     };
 
     async _start() {
@@ -90622,7 +90621,7 @@ class FallBackObject {
   "nodes": [
     {
       "addr": ["webdollar.ddns.net"],
-       "port": 8080,
+       "port": 80,
     },
     {
         "addr": ["192.168.2.0"],
