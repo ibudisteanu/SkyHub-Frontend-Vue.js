f07641f6745389e81e97e0e470ffb1259204eb97
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-04 19:14:53 +0300

    Revert Ready to mine

--- src/client/components/heros/WebDollar.hero.vue
+++ src/client/components/heros/WebDollar.hero.vue
@@ -81,7 +81,7 @@
 
             WebDollar.StatusEvents.emitter.on("blockchain/status", (data)=>{
 
-                if (data.message === "You are already mining...")
+                if (data.message === "Blockchain Ready to Mine")
                     this.loaded = true;
 
             });
