6edb790adfe5e3efe054ff132d8010f81b9503cf
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-04-16 00:50:37 +0300

    change

--- src/client/components/heros/Team.hero.vue
+++ src/client/components/heros/Team.hero.vue
@@ -32,21 +32,6 @@
                             Alexandru is a Forbes Europe 30 under 30 nominee in industry category. In 2013, he was nominated by Time Magazine as one of the most influential teens of the world. MIT named an asteroid with his name.</a>
                         </p>
                     </div>
-                    <div class="our-team panait">
-                        <div class="team-pic">
-                            <img src="/public/assets/images/team/PanaitAlexandru.jpg" alt="Alexandru Ionut Panait" title="Alexandru Ionut Panait">
-                            <div class="over-layer">
-                                <ul class="social-link">
-                                    <li><a href="https://www.facebook.com/Ionut.Alex.Panait" target="_blank"  class="fa fa-facebook"></a></li>
-                                    <li><a href="https://www.linkedin.com/in/panait-alexandru-ionut-64181ba7/" target="_blank" class="fa fa-linkedin2"></a></li>
-                                    <li><a href="https://twitter.com/AlexandruPanai" target="_blank" class="fa fa-twitter"/></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <span class="teamName">Alexandru Ionut Panait</span>
-                        <span class="teamRole">Platform Developer</span>
-                        <p>Alexandru is a young entrepreneur interested in technology and business. General Manager and Founder of a hiring Artificial Intelligence platform and a software agency.</p>
-                    </div>
                     <div class="our-team stratulat">
                         <div class="team-pic">
                             <img src="/public/assets/images/team/Adrian-Stratulat.jpg" alt="Adrian Stratulat">
