dd11301865df8754f5bf16dc4a67b5a4212f3f8c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-16 15:00:29 +0300

    update

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -92637,15 +92637,7 @@ class FallBackObject {
     "nodes": [
 
         {
-            "addr": ["webdollar.ddns.net:80", "webdollar.ddns.net:8081"],
-        },
-        {
-            "addr": ["skyhub.me", "92.222.85.90"],
-            "port": 80,
-        },
-        {
-            "addr": ["presa7.ro"],
-            "port": 443,
+            "addr": ["webdollar.ddns.net:8081"],
         },
 
     ]
