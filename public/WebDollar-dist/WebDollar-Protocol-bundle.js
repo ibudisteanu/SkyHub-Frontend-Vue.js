6c926ed273f21d624b4fc9ec5261dc899511829f
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-24 18:42:03 +0300

    WebDollar Protocol Update

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -27719,16 +27719,9 @@ module.exports = bytesToUuid;
 
 
         {
-            "addr": ["webdollar.ddns.net:80", "webdollar.ddns.net:8081", "webdollar.ddns.net:8082"],
+            "addr": [ "webdollar.ddns.net:8081", "webdollar.ddns.net:8082"],
         },
 
-        {
-            "addr": ["skyhub.me:80"],
-        },
-
-        {
-            "addr": ["robitza.ddns.net:12345"]
-        },
 
     ]
 });
