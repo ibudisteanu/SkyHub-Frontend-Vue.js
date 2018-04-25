241a5b461952260240d2dd75e6d9e228d977a1de
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-25 23:04:15 +0300

    Index template update

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -27745,21 +27745,11 @@ module.exports = bytesToUuid;
     "nodes": [
 
 
-        // {
-        //     "addr": ["webdollar.ddns.net:80", "webdollar.ddns.net:8081", "webdollar.ddns.net:8082"],
-        // },
-        //
-        // {
-        //     "addr": ["skyhub.me:80"],
-        // },
-        //
-        // {
-        //     "addr": ["robitza.ddns.net:12345"]
-        // },
-
         {
-            "addr": ["webdollar.ddns.net:2095"],
+            "addr": ["webdollar.ddns.net:80"],
         },
+
+
 
     ]
 });
