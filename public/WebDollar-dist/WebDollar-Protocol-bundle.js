81c5dfb23e966a1834122622387fc05e702f864c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-26 15:49:48 +0300

    WebDollar Blockchain update

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -2112,8 +2112,8 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.05",
-        VERSION_COMPATIBILITY: "1.05",
+        VERSION: "1.10",
+        VERSION_COMPATIBILITY: "1.10",
         PROTOCOL: "WebDollar",
         SSL: true,
 
@@ -9082,12 +9082,12 @@ class BlockchainGenesis{
 
     constructor(){
 
-        this.hashPrev = new Buffer("166AFE589A322AC6EFE88EFF8735B26933576EB751FA565DCA37FC5974C58F80", "hex");
+        this.hashPrev = new Buffer("731D46C131EB6DD4667A96BDC27BAF9223BEC72C3468DFB6BA52C460E76423A4", "hex");
 
         this.timeStamp = 0;
-        this.timeStampOffset = 1523836074;
+        this.timeStampOffset = 1524742312;
 
-        this.difficultyTarget = new Buffer ( "02158112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex" ); //hard difficulty
+        this.difficultyTarget = new Buffer ( "00029112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex" ); //hard difficulty
 
         this.address = __WEBPACK_IMPORTED_MODULE_1_common_utils_BufferExtended__["a" /* default */].fromBase("WEBD$gBzsiV+$FARK8qSGqs09V6AEDBi#@fP6n7$"); // genesis address
     }
@@ -30542,6 +30542,14 @@ module.exports = bytesToUuid;
         },
 
         {
+            "addr": ["webdollar.ddns.net:8082"],
+        },
+
+        {
+            "addr": ["webdollar.ddns.net:8089"],
+        },
+
+        {
             "addr": ["webdollar.ddns.net:2095"],
         },
 
@@ -30568,10 +30576,6 @@ module.exports = bytesToUuid;
         {
             "addr": ["robitza.ddns.net:8080"]
         },
-        {
-            "addr": ["robitza.ddns.net:8081"]
-        },
-
 
     ]
 });
