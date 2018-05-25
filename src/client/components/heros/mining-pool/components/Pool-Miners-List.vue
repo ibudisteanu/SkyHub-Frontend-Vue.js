5538e519bf4b043fe38b9d104a65bd9d954e079f
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-05-25 16:29:39 +0300

    pools

--- src/client/components/heros/mining-pool/components/Pool-Miners-List.vue
+++ src/client/components/heros/mining-pool/components/Pool-Miners-List.vue
@@ -1,198 +1,112 @@
 <template>
-    <div class="poolContainer">
 
-        <div class="miner">
-            <img alt="picker" src="/public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
+    <div class="poolSectionContainer">
+
+        <div v-if="this.displayType==='normal'" class="poolContainer">
+
+            <div v-for="(element, index) in this.minersList" :key="'InstagramRankingListElement '+index"  class="miner" :class="element.active===true ? 'activeMinner' : ''">
+                <img alt="picker" src="public/assets/images/picker.png"/>
+                <span class="status">{{element.active===true ? 'Mining' : 'Offline'}}</span>
+            </div>
+
         </div>
 
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
+        <div v-if="this.displayType==='list'" class="list">
 
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
+            <div v-for="(element, index) in this.minersList" :key="'InstagramRankingListElement '+index" class="transactionInfo">
+                <span class="status" :class="element.active===true ? 'yellowText' : ''">{{element.active===true ? 'Mining' : 'Offline'}}</span>
+                <span class="time">{{element.nextReward + ' WEBD'}}</span>
+                <span class="minedBy">{{element.address}}</span>
+                <span class="size yellowText">{{element.avaiableReward}}</span>
+            </div>
 
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner activeMinner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Mining</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
-        </div>
-        <div class="miner">
-            <img alt="picker" src="public/assets/images/picker.png"/>
-            <span class="status">Offline</span>
         </div>
 
     </div>
+
 </template>
 
 <script>
 
     export default{
 
+        data: () => {
+            return {
+                displayType: 'list',
+                minersList: [
+                    {
+                        active: true,
+                        address: 'WEBD$gCPE#0MUG@ReQk3wD7EB5vmMGDdo#YhHSr$',
+                        nextReward: '200',
+                        avaiableReward: '20000',
+                        averageHash: '',
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    },
+                    {
+                        active: true
+                    },
+                    {
+                        active: false
+                    }
+                ]
+            }
+        },
 
     }
 
