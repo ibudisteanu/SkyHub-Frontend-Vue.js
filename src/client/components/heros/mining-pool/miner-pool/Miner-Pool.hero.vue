6e63fbabd8e881175c0c161a777b64398dc8e8d6
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-06-17 21:23:23 +0300

    mining pools

--- src/client/components/heros/mining-pool/miner-pool/Miner-Pool.hero.vue
+++ src/client/components/heros/mining-pool/miner-pool/Miner-Pool.hero.vue
@@ -22,11 +22,11 @@
                         <div class="twoButtons">
 
                             <router-link to="/mypool">
-                                <p class="copyPoolLink">Pool Dashboard</p>
+                                <p class="copyPoolLink">Create Your Own Pool</p>
                             </router-link>
 
-                            <p class="copyPoolLink" v-show="this.poolURL !== ''" @click="copyToClipboard">
-                                Copy invite link
+                            <p class="copyPoolLink" @click="copyToClipboard">
+                                Copy invite pool link
                             </p>
 
                         </div>
@@ -34,35 +34,8 @@
                     </div>
 
                 </div>
-                <div class="distributionGrid poolDescription">
 
-                    <div class="verticalAlignMiddle">
-
-                        <span class="oneLineText">
-                            Your Role: <span class="normalSpan yellowColor"> Miner </span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Name: <span class="normalSpan yellowColor"> <a :href="this.poolWebsite"> {{this.poolName}} </a></span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Status: <span class="normalSpan" :class="this.selectStatusColor()">{{ this.minerPoolStatus }}</span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Fee: <span class="normalSpan yellowColor"> <a :href="this.poolWebsite"> {{this.poolFee}} </a></span>
-                        </span>
-                        <span class="oneLineText">
-                            Online Hosts: <span class="normalSpan" :class="this.selectOnlineHostColor()"> {{ this.onlineHosts() }} </span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Hash: <span class="normalSpan yellowColor"> 500 MH/s </span>
-                        </span>
-                        <span class="oneLineText">
-                            Miners: <span class="normalSpan" :class="this.isNotNullColor()"> {{this.poolMinerNumber}} </span>
-                        </span>
-
-                    </div>
-
-                </div>
+                <pool-statistics :statistics="this.statistics"></pool-statistics>
 
             </div>
 
@@ -76,6 +49,7 @@
 
     import Vue from 'vue';
     import Clipboard from 'v-clipboard';
+    import PoolStatistics from '../pool/components/PoolInfo.vue'
 
     Vue.use(Clipboard);
 
@@ -85,99 +59,53 @@
 
         data: () => {
             return {
+                
+                statistics:{
+                    poolName: '',
+                    poolWebsite: '',
+                    poolURL: '',
+                    poolFee: '',
+                    poolServers: {},
+                    poolsList: {},
+                    poolsListSelected: '',
+                    poolStatus: '',
+                    poolMinerNumber: 0
+                },
 
-                minerPoolStatus: '',
                 minerPoolReward: 0,
-
-                poolName: '',
-                poolWebsite: '',
-                poolURL: '',
-                poolDescription: '',
-                poolFee: '',
-
-                poolServers: {},
-
-                poolsList: {},
-                poolsListSelected: '',
-
+             
             }
         },
 
+        components:{PoolStatistics},
+
         methods: {
 
-            numberOfConnectedHosts(){
-
-                var enabledHosts=0;
-
-                for(var i=0;i<=this.poolServers;i++){
-
-                    if(this.poolServers.connected === true) enabledHosts++
-
-                }
-
-                return enabledHosts;
-
-            },
-
             copyToClipboard(){
-                this.$clipboard(this.poolURL);
-            },
-
-            isNotNullColor(){
-
-                if (this.poolMinerNumber===0) return 'redColor';
-
-                return 'greenColor';
-
-            },
-
-            selectStatusColor(){
-
-                if (this.poolStatus==='Started') return 'greenColor';
-                if (this.poolStatus==='Configured') return 'redColor';
-
-                return 'yellowColor';
-
-            },
-
-            selectOnlineHostColor(){
-
-                if(this.numberOfConnectedHosts()===0)  return 'redColor';
-
-                if (Object.keys(this.poolServers).length=== this.numberOfConnectedHosts()) return 'greenColor';
-
-                return 'yellowColor'
-
-            },
-
-            onlineHosts(){
-
-                return this.numberOfConnectedHosts() + ' / ' + Object.keys(this.poolServers).length;
-
+                this.$clipboard(this.statistics.poolURL);
             },
 
             loadPoolData(){
 
                 if ( WebDollar.Blockchain.MinerPoolManagement === undefined){
 
-                    this.minerPoolStatus = "not initialized";
+                    this.statistics.minerPoolStatus = "not initialized";
 
                 } else {
 
-                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolInitialized) this.minerPoolStatus = "initialized";
-                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolOpened) this.minerPoolStatus = "configured";
-                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolStarted) this.minerPoolStatus = "started";
+                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolInitialized) this.statistics.minerPoolStatus = "initialized";
+                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolOpened) this.statistics.minerPoolStatus = "configured";
+                    if (WebDollar.Blockchain.MinerPoolManagement.minerPoolStarted) this.statistics.minerPoolStatus = "started";
 
-                    this.poolFee = Math.floor( WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolFee*100 , 2 );
-                    this.poolURL = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolURL;
-                    this.poolName = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolName;
-                    this.poolWebsite = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolWebsite;
-                    this.poolDescription = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolDescription;
+                    this.statistics.poolFee = Math.floor( WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolFee*100 , 2 );
+                    this.statistics.poolURL = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolURL;
+                    this.statistics.poolName = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolName;
+                    this.statistics.poolWebsite = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolWebsite;
 
                     let poolServers = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolServers;
-                    this.poolServers = WebDollar.Applications.PoolsUtilsHelper.getPoolServersStatus(poolServers);
+                    this.statistics.poolServers = WebDollar.Applications.PoolsUtilsHelper.getPoolServersStatus(poolServers);
 
-                    this.poolsList = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolsList;
+                    this.statistics.poolsList = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolsList;
 
                     let minerPoolFound = false;
 
@@ -185,10 +113,10 @@
 
                         let minerPoolPublicKey = WebDollar.Blockchain.MinerPoolManagement.minerPoolSettings.poolPublicKey.toString("hex");
 
-                        for (let poolPublicKey in this.poolsList){
+                        for (let poolPublicKey in this.statistics.poolsList){
 
                             if (poolPublicKey === minerPoolPublicKey){
-                                this.poolsListSelected = this.poolsList[poolPublicKey].poolName;
+                                this.statistics.poolsListSelected = this.statistics.poolsList[poolPublicKey].poolName;
                                 minerPoolFound = true;
                                 break;
                             }
@@ -198,7 +126,7 @@
                     }
 
                     if (!minerPoolFound)
-                        this.poolsListSelected = 'Pool Mining Disabled';
+                        this.statistics.poolsListSelected = 'Pool Mining Disabled';
 
                 }
 
--- src/client/components/heros/mining-pool/pool/Pool.hero.vue
+++ src/client/components/heros/mining-pool/pool/Pool.hero.vue
@@ -34,36 +34,8 @@
                     </div>
 
                 </div>
-                <div class="distributionGrid poolDescription">
 
-                    <div class="verticalAlignMiddle">
-
-                        <span class="oneLineText">
-                            Your Role: <span class="normalSpan greenColor"> Owner </span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Name: <span class="normalSpan yellowColor"> <a :href="this.poolWebsite"> {{this.poolName}} </a></span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Status: <span class="normalSpan" :class="this.selectStatusColor()">{{ this.poolStatus }}</span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Fee: <span class="normalSpan yellowColor"> <a :href="this.poolWebsite"> {{this.poolFee}} </a></span>
-                        </span>
-                        <span class="oneLineText">
-                            Online Hosts: <span class="normalSpan" :class="this.selectOnlineHostColor()"> {{ this.onlineHosts() }} </span>
-                        </span>
-                        <span class="oneLineText">
-                            Pool Hash: <span class="normalSpan yellowColor"> 500 MH/s </span>
-                        </span>
-                        <span class="oneLineText">
-                            Miners: <span class="normalSpan" :class="this.isNotNullColor()"> {{this.poolMinerNumber}} </span>
-                        </span>
-
-                    </div>
-
-
-                </div>
+                <pool-statistics :statistics="this.statistics"></pool-statistics>
 
             </div>
 
@@ -77,6 +49,7 @@
 
     import Vue from 'vue';
     import Clipboard from 'v-clipboard';
+    import PoolStatistics from '../pool/components/PoolInfo.vue'
 
     Vue.use(Clipboard);
 
@@ -84,98 +57,60 @@
 
         name: 'pool',
 
+        components:{PoolStatistics},
+
         data: () => {
             return {
-                poolStatus: '',
-                poolURL: '',
-                poolFee: 0,
-                poolMinerNumber: 0,
-                poolGlobalHash: 0,
-                poolServers: {},
+
+                statistics:{
+                    poolName: '',
+                    poolWebsite: '',
+                    poolURL: '',
+                    poolFee: '',
+                    poolServers: {},
+                    poolsList: {},
+                    poolsListSelected: '',
+                    poolHash: 0,
+                    poolStatus: '',
+                    poolMinerNumber: 0
+                },
             }
         },
 
         methods: {
 
-            numberOfConnectedHosts(){
-
-                var enabledHosts=0;
-
-                for(var i=0;i<=this.poolServers;i++){
-
-                    if(this.poolServers.connected === true) enabledHosts++
-
-                }
-
-                return enabledHosts;
-
-            },
-
             async handleChangePoolFee(fee){
 
-                this.poolFee = fee;
+                this.statistics.poolFee = fee;
 
                 if (WebDollar.Blockchain.PoolManagement !== undefined)
-                    await WebDollar.Blockchain.PoolManagement.poolSettings.setPoolFee(this.poolFee/100);
+                    await WebDollar.Blockchain.PoolManagement.poolSettings.setPoolFee(this.statistics.poolFee/100);
             },
 
             copyToClipboard(){
                 this.$clipboard(this.poolURL);
             },
 
-            isNotNullColor(){
-
-                if (this.poolMinerNumber===0) return 'redColor';
-
-                return 'greenColor';
-
-            },
-
-            selectStatusColor(){
-
-                if (this.poolStatus==='Started') return 'greenColor';
-                if (this.poolStatus==='Configured') return 'redColor';
-
-                return 'yellowColor';
-
-            },
-
-            selectOnlineHostColor(){
-
-                if(this.numberOfConnectedHosts()===0)  return 'redColor';
-
-                if (Object.keys(this.poolServers).length=== this.numberOfConnectedHosts()) return 'greenColor';
-
-                return 'yellowColor'
-
-            },
-
-            onlineHosts(){
-
-                return this.numberOfConnectedHosts() + ' / ' + Object.keys(this.poolServers).length;
-
-            },
-
             loadPoolData(){
 
                 if (WebDollar.Blockchain.PoolManagement === undefined){
 
-                    this.poolStatus = "not initialized";
+                    this.statistics.poolStatus = "not initialized";
 
                 } else {
 
-                    if (WebDollar.Blockchain.PoolManagement.poolInitialized) this.poolStatus = "Initialized";
-                    if (WebDollar.Blockchain.PoolManagement.poolOpened) this.poolStatus = "Configured";
-                    if (WebDollar.Blockchain.PoolManagement.poolStarted) this.poolStatus = "Started";
+                    if (WebDollar.Blockchain.PoolManagement.poolInitialized) this.statistics.poolStatus = "Initialized";
+                    if (WebDollar.Blockchain.PoolManagement.poolOpened) this.statistics.poolStatus = "Configured";
+                    if (WebDollar.Blockchain.PoolManagement.poolStarted) this.statistics.poolStatus = "Started";
 
                     let poolServers = WebDollar.Blockchain.PoolManagement.poolSettings.poolServers;
-                    this.poolServers = WebDollar.Applications.PoolsUtilsHelper.getPoolServersStatus(poolServers);
+                    this.statistics.poolServers = WebDollar.Applications.PoolsUtilsHelper.getPoolServersStatus(poolServers);
 
                     this.poolURL = WebDollar.Blockchain.PoolManagement.poolSettings.poolURL;
 
-                    this.poolFee = Math.floor( WebDollar.Blockchain.PoolManagement.poolSettings.poolFee*100 , 2 );
+                    this.statistics.poolFee = Math.floor( WebDollar.Blockchain.PoolManagement.poolSettings.poolFee*100 , 2 );
                     if (this.$refs['refPoolFee'] !== undefined)
-                        this.$refs['refPoolFee'].value = this.poolFee;
+                        this.$refs['refPoolFee'].value = this.statistics.poolFee;
 
 
                 }
@@ -188,9 +123,6 @@
         mounted() {
 
             if (typeof window === 'undefined') return;
-
-            Vue.use(Clipboard);
-
 
             WebDollar.StatusEvents.on("pools/status", (data) => {
 
