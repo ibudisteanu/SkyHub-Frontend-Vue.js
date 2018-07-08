95aa822c07b435c5f35233a1fd2d754f86bc90a3
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-07-08 14:50:46 +0300

    New Timeline

--- src/client/components/heros/Timeline.hero.vue
+++ src/client/components/heros/Timeline.hero.vue
@@ -4,13 +4,13 @@
     <div id="timelineSection" >
 
         <span class='alignCenter pageTitle'>TIMELINE</span>
-        <h3>Fast Development of a Browser-Based Cryptocurrency</h3>
+        <h3>Fastest Development of a Browser-Based Cryptocurrency</h3>
 
         <div id="timeline" class="backgroundSection">
             <div class="main-timeline">
                 <div class="timeline">
                     <div class="timeline-icon"><i class="fa fa-file-text2"></i></div>
-                    <span class="year">10 OCT 2017</span>
+                    <span class="year">Q4 2017</span>
                     <div class="timeline-content">
                         <h5 class="title">WHITE PAPER DRAFT</h5>
                         <p class="description">
@@ -20,7 +20,7 @@
                 </div>
                 <div class="timeline">
                     <div class="timeline-icon"><i class="fa fa-trophy"></i></div>
-                    <span class="year">22 OCT 2017</span>
+                    <span class="year">Q4 2017</span>
                     <div class="timeline-content">
                         <h5 class="title">TECH HUB JS HACKATHON</h5>
                         <p class="description">
@@ -30,7 +30,7 @@
                 </div>
                 <div class="timeline">
                     <div class="timeline-icon"><i class="fa fa-earth"></i></div>
-                    <span class="year">DEC 2017</span>
+                    <span class="year">Q4 2017</span>
                     <div class="timeline-content">
                         <h5 class="title">P2P AND MINI-BLOCKCHAIN</h5>
                         <p class="description">
@@ -40,19 +40,29 @@
                 </div>
                 <div class="timeline">
                     <div class="timeline-icon"><i class="fa fa-flask"></i></div>
-                    <span class="year">JAN 2018</span>
+                    <span class="year">Q1 2018</span>
                     <div class="timeline-content">
                         <h5 class="title">LAUNCH TEST NET</h5>
                         <p class="description">
-                            Developing and launching and consensus Proof of Work blockchain
+                            Developing consensus of the Proof of Work and blockchain launching.
                         </p>
                     </div>
                 </div>
                 <div class="timeline timelineUnfinishedMilestones">
                     <div class="timeline-icon"><i class="fa fa-rocket"></i></div>
-                    <span class="year">APRIL 26 2018</span>
+                    <span class="year">Q1 2018</span>
                     <div class="timeline-content">
-                        <h5 class="title">SECURITY + LAUNCH MAIN NET</h5>
+                        <h5 class="title">BUILT IN BOUNTY PROGRAM</h5>
+                        <p class="description">
+                            Development of a bounty program that helped evangelists to spread the word of Webdollar wordlwide.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline timelineUnfinishedMilestones">
+                    <div class="timeline-icon"><i class="fa fa-rocket"></i></div>
+                    <span class="year">Q2 2018</span>
+                    <div class="timeline-content">
+                        <h5 class="title">SECURITY & LAUNCH MAIN NET</h5>
                         <p class="description">
                             Non-Interactive Proofs of Proof-of-Work, Improving the Security and launching a Main Net
                         </p>
@@ -60,17 +70,27 @@
                 </div>
                 <div class="timeline timelineUnfinishedMilestones">
                     <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
-                    <span class="year">JUL 2018</span>
+                    <span class="year">Q3 2018</span>
                     <div class="timeline-content">
-                        <h5 class="title">Mining pools & Exchange</h5>
+                        <h5 class="title">MINING POOLS & REFERRAL SYSTEM</h5>
                         <p class="description">
-                            Developing a easy way to mine with your friends in social mining. And creating the first exchange for ecosystem.
+                            Developing a easy way to mine with your friends in social mining. And creating the first Mining Pools Referral System.
                         </p>
                     </div>
                 </div>
                 <div class="timeline timelineUnfinishedMilestones">
                     <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
-                    <span class="year">AUG-NOV 2018</span>
+                    <span class="year">Q3 2018</span>
+                    <div class="timeline-content">
+                        <h5 class="title">EXCHANGE LISTING</h5>
+                        <p class="description">
+                            Developing the first Exchange for the WebDollar Ecosystem and creating easy to integrate API for other Exchanges
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline timelineUnfinishedMilestones">
+                    <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
+                    <span class="year">Q4 2018</span>
                     <div class="timeline-content">
                         <h5 class="title">EVANGELISM & MARKETING</h5>
                         <p class="description">
@@ -80,11 +100,41 @@
                 </div>
                 <div class="timeline timelineUnfinishedMilestones">
                     <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
-                    <span class="year">NOV-DEC 2018</span>
+                    <span class="year">Q1 2019</span>
                     <div class="timeline-content">
-                        <h5 class="title">SMART CONTRACTS, ANONYMITY</h5>
+                        <h5 class="title">ANONYMITY & MULTISIGN</h5>
                         <p class="description">
-                            Developing a Smart Contracts Platform, Anonymity, Currency Exchange, multisign with fingerprint / faceID
+                            Developing Anonymity and researching multisign with fingerprint/faceID
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline timelineUnfinishedMilestones">
+                    <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
+                    <span class="year">Q2 2019</span>
+                    <div class="timeline-content">
+                        <h5 class="title">SMART CONTRACTS</h5>
+                        <p class="description">
+                            Developing a Smart Contracts Platform that allows  users to create their own financial assets — Webdollar Tokens.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline timelineUnfinishedMilestones">
+                    <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
+                    <span class="year">Q4 2019</span>
+                    <div class="timeline-content">
+                        <h5 class="title">DECENTRALIZED BAZZAR</h5>
+                        <p class="description">
+                            Creation of a decentralized Bazaar where people can buy or sell goods using Webdollar as a payment method.
+                        </p>
+                    </div>
+                </div>
+                <div class="timeline timelineUnfinishedMilestones">
+                    <div class="timeline-icon unfinishedMilestones"><i class="fa fa-gift"></i></div>
+                    <span class="year">Q4 2020</span>
+                    <div class="timeline-content">
+                        <h5 class="title">SCALING TRANSACTIONS</h5>
+                        <p class="description">
+                            Improving the max number of transactions per second accepted by the WebDollar Protocol
                         </p>
                     </div>
                 </div>
