7bc857720e77c842f2acde378559c3928c08983f
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-19 06:20:40 +0300

    fixing sync optimization #4

--- public/WebDollar-dist/WebDollar-Protocol-bundle.js
+++ public/WebDollar-dist/WebDollar-Protocol-bundle.js
@@ -27813,7 +27813,7 @@ class InterfaceBlockchainTransactionsWizard{
     }
 
     calculateFeeSimple(){
-        return 10000;
+        return 10*10000;
         //return this.calculateFeeWizzard( new Buffer(141) );
     }
 
