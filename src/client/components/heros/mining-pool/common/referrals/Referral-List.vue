ea974f646742781b8e85e554850d87e9385b588a
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-08-01 01:41:52 +0300

    Merge remote-tracking branch 'remotes/origin/master' into MiningPools
    # Conflicts:
    #	Dockerfile
    #	public/WebDollar-dist/WebDollar-Protocol-bundle.js
    #	public/WebDollar-dist/WebDollar-Protocol-bundle.js.map
    #	public/WebDollar-dist/WebDollar-User-Interface-bundle.js
    #	public/WebDollar-dist/WebDollar-User-Interface-bundle.js.map
    #	public/assets/images/media/WebDollar-Eu-Parliament.jpeg
    #	public/assets/images/team/ConstantinBusuioceanu.jpeg
    #	public/assets/images/team/CristianAntal.jpg
    #	public/assets/images/team/DanielMaxer.jpg
    #	public/assets/images/team/DeaconuMadalin.jpg
    #	public/assets/images/team/GeorgeAlexandruSfica.jpg
    #	src/client/components/heros/Blockchain-Distribution.hero.vue
    #	src/client/components/heros/Team.hero.vue
    #	src/index.template.html

--- src/client/components/heros/mining-pool/common/referrals/Referral-List.vue
+++ src/client/components/heros/mining-pool/common/referrals/Referral-List.vue
@@ -2,55 +2,64 @@
 
     <div class="poolSectionContainer">
 
-        <div v-if="this.displayType==='normal'" class="poolContainer">
+        <div v-if="this.referralData.referees.length>0">
 
-            <div v-for="(element, index) in this.referralData.referees" :key="'miningPoolListElement '+index"  class="miner" :class="element.active===true ? 'activeMiner' : ''"  @click="selectReferralMiner(index)">
-                <img alt="picker" src="/public/assets/images/picker.png"/>
-                <span class="status">{{element.active===true ? 'Mining' : 'Offline'}}</span>
+            <div v-if="this.displayType==='normal'" class="poolContainer">
+
+                <div v-for="(element, index) in this.referralData.referees" :key="'miningPoolListElement '+index"  class="miner" :class="element.active===true ? 'activeMiner' : ''"  @click="selectReferralMiner(index)">
+                    <img alt="picker" src="/public/assets/images/picker.png"/>
+                    <img class="walletListImage normalListImage" :src="getWalletImage(element.address)" :title="element.address"/>
+                    <span class="status">{{element.active===true ? 'Mining' : 'Offline'}}</span>
+                </div>
+
+            </div>
+
+            <div v-if="this.displayType==='list'" class="list">
+
+                <div class="transactionInfo referralInfo tableHeader">
+
+                    <span>Status</span>
+                    <span>Miner Address</span>
+                    <span>Next Reward</span>
+                    <span>Total Reward</span>
+
+                </div>
+
+                <div v-for="(element, index) in this.referralData.referees" :key="'miningPoolListElement '+index" @click="selectReferralMiner(index)" class="transactionInfo referralInfo">
+
+                    <div class="mobileTableShow">
+                        <span class="mobileTableShowTitle">Status:</span>
+                        <span class="status" :class="element.online ? 'yellowText' : ''">{{element.active ? 'Mining' : 'Offline'}}</span>
+                    </div>
+
+                    <div class="mobileTableShow">
+                        <span class="mobileTableShowTitle">Miner Address:</span>
+                        <img class="walletListImage" :src="getWalletImage(element.address)" :title="element.address"/>
+                    </div>
+
+                    <div class="mobileTableShow">
+                        <span class="mobileTableShowTitle">Next Reward:</span>
+                        <span class="time">{{getNextReward(element) }} WEBD</span>
+                    </div>
+
+                    <div class="mobileTableShow">
+                        <span class="mobileTableShowTitle">Sent Reward:</span>
+                        <span class="minedBy">{{getSentReward(element)}} WEBD</span>
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
 
-        <div v-if="this.displayType==='list'" class="list">
+        <div v-if="this.referralData.referees === undefined">
 
-            <div class="transactionInfo referralInfo tableHeader">
+            <span>You don't have referrals yet</span>
 
-                <span>Status</span>
-                <span>Referral Address</span>
-                <span>Miner Address</span>
-                <span>Next Reward</span>
-                <span>Total Reward</span>
-
-            </div>
-
-            <div v-for="(element, index) in this.referralData.referees" :key="'miningPoolListElement '+index" @click="selectReferralMiner(index)" class="transactionInfo referralInfo">
-
-                <div class="mobileTableShow">
-                    <span class="mobileTableShowTitle">Status:</span>
-                    <span class="status" :class="element.online ? 'yellowText' : ''">{{element.active ? 'Mining' : 'Offline'}}</span>
-                </div>
-
-                <div class="mobileTableShow">
-                    <span class="mobileTableShowTitle">Referral Address:</span>
-                    <img class="walletListImage" :src="getWalletImage(element.address)" :title="element.address"/>
-                </div>
-
-                <div class="mobileTableShow">
-                    <span class="mobileTableShowTitle">Miner Address:</span>
-                    <img class="walletListImage" :src="getWalletImage(element.referralAddress)" :title="element.referralAddress"/>
-                </div>
-
-                <div class="mobileTableShow">
-                    <span class="mobileTableShowTitle">Next Reward:</span>
-                    <span class="time">{{getNextReward(element) }} WEBD</span>
-                </div>
-
-                <div class="mobileTableShow">
-                    <span class="mobileTableShowTitle">Sent Reward:</span>
-                    <span class="minedBy">{{getSentReward(element)}} WEBD</span>
-                </div>
-
-            </div>
+            <p class="copyPoolLink" @click="copyToClipboard">
+                Copy Referral Link
+            </p>
 
         </div>
 
@@ -59,6 +68,10 @@
 </template>
 
 <script>
+
+    import Clipboard from 'v-clipboard';
+
+    Vue.use(Clipboard);
 
     export default{
 
@@ -69,6 +82,7 @@
         data:()=>{
             return {
                 referralData: {},
+                poolURLReferral: '',
             }
         },
 
@@ -101,7 +115,11 @@
 
               return element.sent /   WebDollar.Applications.CoinsHelper.WEBD;
 
-            }
+            },
+
+            copyToClipboard(){
+                this.$clipboard(this.poolURLReferral);
+            },
 
         },
 
@@ -113,8 +131,6 @@
         mounted(){
 
             if (typeof window !== "undefined") return;
-
-
 
         }
 
@@ -129,7 +145,19 @@
     }
 
     .referralInfo{
-        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+
+    .normalListImage{
+        display: block!important;
+        margin-left: 16px;
+        margin-top: 15px;
+    }
+
+    .miner .status{
+        margin-top: 0;
+        line-height: 2;
+        margin-top: 0!important;
     }
 
 </style>
